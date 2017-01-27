ff953c3fdb6711482a95bd19df3099bf990efbc3
David Esposito <david.esposito@bettercloud.com>
2017-01-27 10:18:28 -0500

    Adding statsd logger
    Signed-off-by: David Esposito <david.esposito@bettercloud.com>

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,8 +3,14 @@ package com.example;
 import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
+import org.springframework.boot.actuate.endpoint.MetricsEndpoint;
+import org.springframework.boot.actuate.endpoint.MetricsEndpointMetricReader;
 import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.statsd.StatsdMetricWriter;
+import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +20,7 @@ import java.util.concurrent.TimeUnit;
 
 @RestController
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -39,15 +45,25 @@ public class DemoApplication {
 		return true;
 	}
 
-	@Bean
-	public ConsoleReporter metricWriter(MetricRegistry reg) {
-		ConsoleReporter writer = ConsoleReporter.forRegistry(reg)
-				.convertRatesTo(TimeUnit.SECONDS)
-				.convertDurationsTo(TimeUnit.MILLISECONDS)
-				.build();
+    @Bean
+    public MetricsEndpointMetricReader metricsEndpointMetricReader(MetricsEndpoint metricsEndpoint) {
+        return new MetricsEndpointMetricReader(metricsEndpoint);
+    }
 
-		writer.start(10, TimeUnit.SECONDS);
+    @Bean
+    @ExportMetricWriter
+    public MetricWriter statsdWriter() {
+        String prefix = (int) (Math.random() * Integer.MAX_VALUE) + "";
+        return new StatsdMetricWriter(prefix, "localhost", 8125);
+    }
 
-		return writer;
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        ConsoleReporter writer = ConsoleReporter.forRegistry(reg)
+                .convertRatesTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+
+        writer.start(10, TimeUnit.SECONDS);
+    }
 }
